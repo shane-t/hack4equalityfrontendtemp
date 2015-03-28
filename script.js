7bc60376db45cde815e1f1ebe23bed750a44fab8
Ubuntu <ubuntu@ip-172-31-39-118.eu-west-1.compute.internal>
2015-03-28 12:44:46 +0000

    more things

---
+++ script.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+
+   var ui = {
+     townTo : $('#town_to'),
+     townFrom : $('#town_from'),
+     canvas : $('#map_canvas')
+   };
+
+
+
+
+   window.ui = ui;
+
+}());
